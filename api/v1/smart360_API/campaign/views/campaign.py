86408e0fb5903564381207af6af307f1f021122f
MCCIA <kacharepriya17@gmail.com>
2020-04-22 14:14:53 +0530

    Add API for campaign

--- api/v1/smart360_API/campaign/views/campaign.py
+++ api/v1/smart360_API/campaign/views/campaign.py
@@ -3,22 +3,19 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from api.v1.smart360_API.campaign.views.common_functions import is_token_valid, get_payload, \
-    get_filtered_campaign, check_authorization, get_user
+from api.v1.smart360_API.registration.views.common_functions import is_token_valid, get_payload, \
+     check_authorization, get_user
 
 from api.v1.smart360_API.lookup.models.privillege import Privillege
 from api.v1.smart360_API.lookup.models.sub_module import SubModule
-from api.v1.smart360_API.smart360_API.settings import DISPLAY_DATE_FORMAT
+from api.v1.smart360_API.campaign.models.campaign_master import CampaignMaster
 
 
+# Api for add campaign details
+class AddCampaignApi(APIView):
 
-# Api for getting campaign list, filter, search
-class CampaignListApiView(APIView):
-
-    def get(self, request, format=None):
+    def post(self, request, format=None):
         try:
-            campaign_list = []
-
             # Checking authentication start
             if is_token_valid(request.data['token']):
                 payload = get_payload(request.data['token'])
@@ -29,29 +26,50 @@ class CampaignListApiView(APIView):
                 sub_module = SubModule.objects.filter(id = 1)
                 if check_authorization(user, privillege, sub_module):
 
-                     # Code for filtering campaign start
-                    campaigns = get_filtered_campaign(user, request)
+                    # Code for add campaign start
+                    # first check values are present or not
+                    if request.data['campaign_name'] and request.data['campaign_type'] and request.data['area'] and\
+                          request.data['sub_area'] and request.data['start_date'] and request.data['end_date']:
 
-                    # Code for sending campaigns in response
-                    for campaign in campaigns:
-                        campaign_list.append({
-                            'cam_group' : campaign.cam_group,
-                            'name' : campaign.name,
-                            'status' : Status.objects.get(id = campaign.status_id).status,
-                            'description' : campaign.description,
-                            'budget_amount' : campaign.budget_amount,
-                            'actual_amount' : campaign.actual_amount,
-                            'area' : Area.objects.get(id = campaign.area_id).area,
-                            'sub_area' : SubArea.objects.get(id = campaign.sub_area_id).sub_area,
-                            'category': Category.objects.get(id=campaign.category_id).category,
-                            'sub_category': SubCategory.objects.get(id=campaign.sub_category_id).sub_category,
-                            'created_on' : campaign.created_date.strftime(DISPLAY_DATE_FORMAT)
-                        })
-                    return Response({
-                        'success': 'true',
-                        'data': campaign_list,
-                        'message': 'Data sent successfully.'
-                    }, status=status.HTTP_200_OK)
+                        # check Campaign is Already Exists or not
+                        if CampaignMaster.objects.get(area=request.data['area'],sub_area=request.data['sub_area'],
+                                                      start_date=request.data['start_date'],end_date=request.data['end_date']):
+
+                            campaign_data = {'campaign_name':request.data['campaign_name'],'area':request.data['area'],
+                                             'sub_area':request.data['sub_area'],'start_date':request.data['start_date'],'end_date':request.data['end_date']}
+
+                            return Response({
+                                'success': 'false',
+                                'data': campaign_data,
+                                'message': 'Campaign Already Exists.'
+                            }, status=status.HTTP_409_CONFLICT)
+                        else:
+
+                            campaignmaster = CampaignMaster(
+                                tenant=TenantMaster.objects.get(id=request.data['tenant_id']),
+                                utility=UtilityMaster.objects.get(id=request.data['utility']),
+                                name=request.data['campaign_name'],
+                                cam_group_id=request.data['cam_gr_id_string'],
+                                category_id=request.data['category_id_string'],
+                                sub_category_id=request.data['sub_cat_id_string'],
+                                start_date=request.data['start_date'],
+                                end_date=request.data['end_date'],
+                                doc_url=request.data['document_url'],
+                                description=request.data['description'],
+                            )
+                            campaignmaster.save()
+
+                            return Response({
+                                'success': 'true',
+                                'data': '',
+                                'message': 'Data Save Successfully.'
+                            }, status=status.HTTP_200_OK)
+                    else:
+                        return Response({
+                            'success': 'false',
+                            'data': '',
+                            'message': 'Some values are missing'
+                        }, status=status.HTTP_400_BAD_REQUEST)
 
                 else:
                     return Response({
--- api/v1/smart360_API/campaign/views/common_functions.py
+++ api/v1/smart360_API/campaign/views/common_functions.py
@@ -1,35 +1,7 @@
 import jwt
 
-from api.v1.smart360_API.campaign.models.compaign_master import CampaignMaster
 from api.v1.smart360_API.smart360_API.settings import SECRET_KEY
 
-
-
-def get_filtered_campaign(request, user):
-    compaign = CampaignMaster.objects.filter(tenant_id=user.tenant_id,
-                                                utility_id__in=user.data_access.all())
-    if request.data['utillity']:
-        compaign = compaign.objects.filter(utility_id=
-                                                     request.data['utillity'])
-    if request.data['category']:
-        compaign = compaign.objects.filter(consumer_category_id=
-                                                     request.data['category'])
-    if request.data['sub_category']:
-        compaign = compaign.objects.filter(sub_category_id=
-                                                     request.data['sub_category'])
-    if request.data['cam_group']:
-        compaign = compaign.objects.filter(cam_group= request.data['cam_group'])
-
-    if request.data['area']:
-        compaign = compaign.objects.filter(area_id=
-                                                     request.data['area'])
-    if request.data['subarea']:
-        compaign = compaign.objects.filter(subarea_id=
-                                                     request.data['subarea'])
-    if request.data['status']:
-        compaign = compaign.objects.filter(status_id=
-                                                     request.data['status'])
-    return compaign
 
 def is_token_valid(token):
     return Token.objects.filter(token=token).exists()
