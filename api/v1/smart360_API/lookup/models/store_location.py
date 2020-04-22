208e7de33c5330e4d9d3f4a54412bbbb4f329ea8
swanandi112@gmail.com <swanandi112@gmail.com>
2020-04-22 19:10:33 +0530

    Purchase Lookup model updated.

--- api/v1/smart360_API/lookup/models/store_location.py
+++ api/v1/smart360_API/lookup/models/store_location.py
@@ -1,10 +1,10 @@
-# table: Route Details
+# table: Store Location
 # table type: lookup
-# table name: 2.12.50 Route Details (Local)
-# table description: It is lookup tables to be used in tables.
+# table name: 2.12.91 Store Location (Local)
+# table description: It store the Store Location .
 # frequency of data changes: Medium
 # sample table data:
-# reference tables: 2.3.8.2 Reading Consumer Master, 2.3.8.3 Jobcard, 2.3.8.4 Meter Reading, 2.3.9 Invoice/Bill
+# reference tables: 2.7.10 Store Master
 # auther: Gauri
 # creation date: 22/4/2020
 
@@ -17,16 +17,13 @@ import uuid  # importing package for GUID
 from django.db import models  # importing package for database
 
 
-#Create RouteDetails table start
-class RouteDetails(models.Model):
+#Create StoreLocation table start
+class StoreLocation(models.Model):
     id_string = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
     tenant = models.ForeignKey(TenantMaster, null=False, blank=False)
     utility = models.ForeignKey(UtilityMaster, null=False, blank=False)
-    route_code = models.CharField(null=False, blank=False)
-    bill_month = models.IntegerField(null=False, blank=False)
-    bill_cycle = models.CharField(null=False, blank=False)
-    bill_status = models.CharField(null=False, blank=False)
-    mru_master = models.CharField(null=False, blank=False)
+    store_location = models.CharField(null=False, blank=False)
+    city_id = models.CharField(null=False, blank=False)
     created_by = models.CharField(null=False, blank=False)
     updated_by = models.CharField(null=False, blank=False)
     created_date = models.DateTime(null=True, blank=True, default=datetime.now())
@@ -34,9 +31,9 @@ class RouteDetails(models.Model):
     is_active = models.Boolean(default=False)
 
     def __str__(self):
-        return self.route_code
+        return self.store_location
 
     def __unicode__(self):
-        return self.route_code
+        return self.store_location
 
- # Create RouteDetails table end
+ # Create StoreLocation table end
--- api/v1/smart360_API/lookup/models/tender_status.py
+++ api/v1/smart360_API/lookup/models/tender_status.py
@@ -1,10 +1,10 @@
-# table: Route Details
+# table:  Tender Status
 # table type: lookup
-# table name: 2.12.50 Route Details (Local)
-# table description: It is lookup tables to be used in tables.
-# frequency of data changes: Medium
-# sample table data:
-# reference tables: 2.3.8.2 Reading Consumer Master, 2.3.8.3 Jobcard, 2.3.8.4 Meter Reading, 2.3.9 Invoice/Bill
+# table name: 2.12.89 Tender Status (Local)
+# table description: It store the Tender status.
+# frequency of data changes: low
+# sample table data: "Initiated","Received","Approved","Rejected","Pending","Hold"
+# reference tables:2.7.3 Tender Master
 # auther: Gauri
 # creation date: 22/4/2020
 
@@ -17,16 +17,12 @@ import uuid  # importing package for GUID
 from django.db import models  # importing package for database
 
 
-#Create RouteDetails table start
-class RouteDetails(models.Model):
+#Create TenderStatus table start
+class TenderStatus(models.Model):
     id_string = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
     tenant = models.ForeignKey(TenantMaster, null=False, blank=False)
     utility = models.ForeignKey(UtilityMaster, null=False, blank=False)
-    route_code = models.CharField(null=False, blank=False)
-    bill_month = models.IntegerField(null=False, blank=False)
-    bill_cycle = models.CharField(null=False, blank=False)
-    bill_status = models.CharField(null=False, blank=False)
-    mru_master = models.CharField(null=False, blank=False)
+    status = models.CharField(null=False, blank=False)
     created_by = models.CharField(null=False, blank=False)
     updated_by = models.CharField(null=False, blank=False)
     created_date = models.DateTime(null=True, blank=True, default=datetime.now())
@@ -34,9 +30,9 @@ class RouteDetails(models.Model):
     is_active = models.Boolean(default=False)
 
     def __str__(self):
-        return self.route_code
+        return self.status
 
     def __unicode__(self):
-        return self.route_code
+        return self.status
 
- # Create RouteDetails table end
+ # Create TenderStatus table end
