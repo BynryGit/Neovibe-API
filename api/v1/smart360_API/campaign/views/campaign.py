ffff0bf3b7b6216a884d45cfb5c410019b5e21e3
MCCIA <kacharepriya17@gmail.com>
2020-04-23 09:53:01 +0530

    change in campaign table

--- api/v1/smart360_API/campaign/views/campaign.py
+++ api/v1/smart360_API/campaign/views/campaign.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from api.v1.smart360_API.campaign.views.common_functions import is_token_valid, get_payload, \
-     check_authorization, get_user
+     check_authorization, get_user,get_filtered_campaign
 
 from api.v1.smart360_API.smart360_API.messages import STATE,SUCCESS,ERROR,EXCEPTION
 from api.v1.smart360_API.lookup.models.privilege import Privilege
@@ -27,6 +27,53 @@ from api.v1.smart360_API.campaign.models.campaign_master import CampaignMaster
 
 # Api for add campaign details
 class AddCampaignApi(APIView):
+
+    def get(self, request, format=None):
+        try:
+            campaign_list = []
+            # Checking authentication start
+            if is_token_valid(request.data['token']):
+                payload = get_payload(request.data['token'])
+                user = get_user(payload['id_string'])
+
+                # Checking authorization start
+                privillege = Privillege.objects.filter(id=1)
+                sub_module = SubModule.objects.filter(id=1)
+                if check_authorization(user, privillege, sub_module):
+
+                    # Code for filtering campaign start
+                    campaigns = get_filtered_campaign(user, request)
+
+                    # Code for sending campaigns in response
+                    for campaign in campaigns:
+                        campaign_list.append({
+                            'cam_type': CampaignGroup.objects.get(id=campaign.type_id).campaign_type,
+                            'status' : Status.objects.get(id_string = registration.status_id).status_name,
+                            'category': Category.objects.get(id=campaign.category_id).category,
+                        })
+                    return Response({
+                        'success': 'true',
+                        'data': campaign_list,
+                        'message': 'Data sent successfully.'
+                    }, status=status.HTTP_200_OK)
+
+                else:
+                    return Response({
+                        'success': 'false',
+                        'data': '',
+                        'message': 'User does not have required privillege.',
+                    }, status=status.HTTP_401_UNAUTHORIZED)
+            else:
+                return Response({
+                    'success': 'false',
+                    'data': '',
+                    'message': 'Please login first.'
+                }, status=status.HTTP_401_UNAUTHORIZED)
+        except Exception as e:
+            return Response({
+                'success': 'false',
+                'error': str(traceback.print_exc(e))
+            }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
     def post(self, request, format=None):
         try:
@@ -60,16 +107,14 @@ class AddCampaignApi(APIView):
                             # save campaign details
                             campaignmaster = CampaignMaster(
                                 tenant=TenantMaster.objects.get(id=request.data['tenant_id']), #TODO:  Wrapper
-                                utility=UtilityMaster.objects.get(id=request.data['utility']), #TODO: Change to utility_id
+                                utility=UtilityMaster.objects.get(id=request.data['utility_id']),
                                 name=request.data['campaign_name'],
                                 cam_group_id=request.data['cam_gr_id_string'],
                                 category_id=request.data['category_id_string'], #TODO: Write function to get category by IDstring.
                                 sub_category_id=request.data['sub_cat_id_string'],
                                 start_date=request.data['start_date'],
                                 end_date=request.data['end_date'],
-                                portion_id=request.data['portion_id_string'], #TODO:  to be removed
-                                mru_id=request.data['mru_id_string'], #TODO:  to be removed
-                                doc_url=request.data['document_url'], ##TODO:  use global method
+                                # doc_url=request.data['document_url'], ##TODO:  use global method
                                 description=request.data['description'], #TODO: limit to be added. txt -250, desc -1000
                             )
                             campaignmaster.save()
