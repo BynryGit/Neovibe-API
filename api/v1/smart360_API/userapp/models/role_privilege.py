c8b933c10db0cd542785ba69aadf6778aee14b2c
nileshgudhe <ceo@bynry.com>
2020-04-25 19:51:57 +0530

    changes to User tables

--- api/v1/smart360_API/userapp/models/role_privilege.py
+++ api/v1/smart360_API/userapp/models/role_privilege.py
@@ -25,13 +25,10 @@ class RolePrivilege(models.Model):
     id_string = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
     tenant = models.ForeignKey(TenantMaster, blank=False, null=False)
     utility = models.ForeignKey(UtilityMaster, blank=False, null=False)
-    role = models.IntegerField(null=False, blank=False)
-    department = models.CharField(max_lengt=200, null=True, blank=True)
-    role_type = models.IntegerField(null=False, blank=False)
+    role_id = models.IntegerField(null=False, blank=False)
     module = models.IntegerField(null=False, blank=False)
     sub_module = models.IntegerField(null=False, blank=False)
-    form_factor = models.IntegerField(null=False, blank=False)
-    status = models.IntegerField(null=False, blank=False)
+    previlege_id = models.IntegerField(null=False, blank=False)
     is_active = models.BooleanField(default=False)
     created_by = models.IntegerField(null=True, blank=True)
     updated_by = models.IntegerField(null=True, blank=True)
@@ -39,9 +36,9 @@ class RolePrivilege(models.Model):
     updated_date = models.DateField(null=True, blank=True, default=datetime.now())
 
     def __str__(self):
-        return self.role_type
+        return self.roleprivilege.id
 
     def __unicode__(self):
-        return self.role_type
+        return self.self.roleprivilege.id
 
 # Create Role Privilege table end
