79875488e9c4bed0f5d3723ca6e3696d66c069ec
Rohan Wagh <waghrohan218@gmail.com>
2020-04-29 17:45:10 +0530

    consumer

--- api/v1/consumer/models/consumer_ownership.py
+++ api/v1/consumer/models/consumer_ownership.py
@@ -10,8 +10,8 @@ from django.db import models  # importing package for database
 
 class ConsumerOwnership(models.Model):
     id_string = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
-    tenant = models.ForeignKey(TenantMaster, null=False, blank=False)
-    utility = models.ForeignKey(UtilityMaster, null=False, blank=False)
+    tenant = models.ForeignKey(TenantMaster, blank=True, null=True, on_delete=models.SET_NULL)
+    utility = models.ForeignKey(UtilityMaster, blank=True, null=True, on_delete=models.SET_NULL)
     ownership = models.CharField(max_length=200, blank=False, null=False)
     is_active = models.BooleanField(default=False)
     created_by = models.BigIntegerField(null=True, blank=True)
