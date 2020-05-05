232ee2e8fdaea3f225e891d8c79a74706a7da01d
Rohan Wagh <waghrohan218@gmail.com>
2020-05-05 18:34:36 +0530

    fixtures update

--- api/v1/registration/views/registrations.py
+++ api/v1/registration/views/registrations.py
@@ -1,5 +1,4 @@
 import traceback
-
 from django.db import transaction
 from rest_framework import status
 from rest_framework.response import Response
@@ -59,7 +58,7 @@ class RegistrationList(APIView):
                     # Checking authorization end
 
                     # Code for filtering registrations start
-                    user = SystemUser.objects.get(id=2)
+                    user = UserDetail.objects.get(id=2)
                     registrations, total_pages, page_no, result, error = get_filtered_registrations(user, request)
                     if result == False:
                         return Response({
@@ -228,11 +227,7 @@ class Registration(APIView):
                         # Request data verification end
 
                         # Save basic and payment details start
-<<<<<<< HEAD
-                        user = SystemUser.objects.get(id=2)
-=======
-                        user = UserDetail.objects.get(id=3)
->>>>>>> a36c50534ff2f3c7c6f0ed130c5ceb0d159e7751
+                        user = UserDetail.objects.get(id=2)
                         sid = transaction.savepoint()
                         registration, result = add_basic_registration_details(request, user, sid)
                         if result == False:
@@ -289,11 +284,8 @@ class Registration(APIView):
                         # Request data verification end
 
                         # Save basic details start
-<<<<<<< HEAD
-                        user = SystemUser.objects.get(id=2)
-=======
-                        user = UserDetail.objects.get(id=3)
->>>>>>> a36c50534ff2f3c7c6f0ed130c5ceb0d159e7751
+                        user = UserDetail.objects.get(id=2)
+
                         registration, result = save_edited_basic_registration_details(request, user)
                         if result == False:
                             return Response({
