a57572e2cccc814bceddf0ab68f685bce26d295d
arpita <arpita.badwaik@bynry.com>
2020-05-12 18:25:43 +0530

    changes in role apis according to new serializer

--- api/v1/commonapp/serializers/form_factor.py
+++ api/v1/commonapp/serializers/form_factor.py
@@ -1,32 +1,25 @@
 from rest_framework import serializers
 
-from v1.commonapp.models.department import Department
+from v1.commonapp.models.form_factor import FormFactor
 
 
-class DepartmentSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Department
-        fields = ('status', 'id_string')
-
-
-class DepartmentListSerializer(serializers.ModelSerializer):
+class FormFactorListSerializer(serializers.ModelSerializer):
     tenant = serializers.ReadOnlyField(source='tenant.name')
     tenant_id_string = serializers.ReadOnlyField(source='tenant.id_string')
     utility = serializers.ReadOnlyField(source='utility.name')
     utility_id_string = serializers.ReadOnlyField(source='utility.id_string')
 
     class Meta:
-        model = Department
+        model = FormFactor
         fields = ('id_string', 'tenant', 'tenant_id_string', 'utility', 'utility_id_string', 'name', 'is_active')
 
 
-class DepartmentViewSerializer(serializers.ModelSerializer):
+class FormFactorViewSerializer(serializers.ModelSerializer):
     tenant = serializers.ReadOnlyField(source='tenant.name')
     tenant_id_string = serializers.ReadOnlyField(source='tenant.id_string')
     utility = serializers.ReadOnlyField(source='utility.name')
     utility_id_string = serializers.ReadOnlyField(source='utility.id_string')
 
     class Meta:
-        model = Department
+        model = FormFactor
         fields = ('id_string', 'tenant', 'tenant_id_string', 'utility', 'utility_id_string', 'name', 'is_active')
--- api/v1/commonapp/urls.py
+++ api/v1/commonapp/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
-from v1.commonapp.views.role import RoleType, RoleSubType, FormFactor, Department
+
+from v1.commonapp.views.department import Department, DepartmentList
+from v1.commonapp.views.form_factor import FormFactor, FormFactorList
+from v1.commonapp.views.role_type import RoleType, RoleSubType
+from v1.commonapp.views.sub_modules import SubModule
 
 urlpatterns = [
-    path('roletype/', RoleType.as_view()),
-    path('rolesubtypes/', RoleSubType.as_view()),
-    path('departments/', FormFactor.as_view()),
-    path('submodules/', Department.as_view())
+    path('role_type/', RoleType.as_view()),
+    path('role_subtypes/', RoleSubType.as_view()),
+    path('form_factors/', FormFactor.as_view()),
+    path('form_factor/list/', FormFactorList.as_view()),
+    path('departments/', Department.as_view()),
+    path('department/list/', DepartmentList.as_view()),
+    path('submodules/', SubModule.as_view())
 ]
