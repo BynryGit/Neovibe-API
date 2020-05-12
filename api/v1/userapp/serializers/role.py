4a3670ca817c08519fc9aaedf7bc1de3ebbac91a
arpita <arpita.badwaik@bynry.com>
2020-05-12 14:20:05 +0530

    user list api

--- api/v1/userapp/serializers/role.py
+++ api/v1/userapp/serializers/role.py
@@ -1,11 +1,20 @@
 from rest_framework import serializers
+
+from v1.userapp.models.role_status import RoleStatus
 from v1.userapp.models.user_role import UserRole
 
 
+class RoleStatusSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = RoleStatus
+        fields = ('status', 'id_string')
+
+
 class RoleListSerializer(serializers.ModelSerializer):
+    status = RoleStatusSerializer(many=False,required=True,source='get_role_status')
     role_type = serializers.ReadOnlyField(source='get_role_type')
     role_sub_type = serializers.ReadOnlyField(source='get_role_sub_type')
-    status = serializers.ReadOnlyField(source='get_role_status')
     form_factor = serializers.ReadOnlyField(source='get_form_factor')
     department = serializers.ReadOnlyField(source='get_department')
 
--- api/v1/userapp/serializers/user.py
+++ api/v1/userapp/serializers/user.py
@@ -1,10 +1,18 @@
 from rest_framework import serializers
 
 from v1.userapp.models.user_master import UserDetail
+from v1.userapp.models.user_status import UserStatus
+
+
+class UserStatusSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = UserStatus
+        fields = ('status', 'id_string')
 
 
 class UserListSerializer(serializers.ModelSerializer):
-    status = serializers.ReadOnlyField(source='get_user_status')
+    status = UserStatusSerializer(many=False,required=True,source='get_user_status')
     department = serializers.ReadOnlyField(source='get_department')
     role = serializers.ReadOnlyField(source='get_user_role')
 
