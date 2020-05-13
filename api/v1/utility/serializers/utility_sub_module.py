555cee35b99d5a00151db69df1aff4c4095be0dc
Akshay <akshay.nibrad@bynry.com>
2020-05-13 18:28:15 +0530

    utility api

--- api/v1/utility/serializers/utility_sub_module.py
+++ api/v1/utility/serializers/utility_sub_module.py
@@ -1,6 +1,8 @@
 __author__ = "aki"
 
 from rest_framework import serializers
+from django.db import transaction
+from django.utils import timezone
 from v1.commonapp.serializers.tenant import TenantMasterViewSerializer
 from v1.commonapp.serializers.utility import UtilityMasterViewSerializer
 from v1.utility.models.utility_sub_module import UtilitySubModule as UtilitySubModuleTbl
@@ -14,3 +16,25 @@ class UtilitySubModuleViewSerializer(serializers.ModelSerializer):
     class Meta:
         model = UtilitySubModuleTbl
         fields = ('id_string', 'submodule_name', 'submodule_desc', 'utility_module', 'tenant', 'utility',)
+
+
+class UtilitySubModuleSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = UtilitySubModuleTbl
+        fields = ('submodule_name', 'submodule_desc')
+
+    def create(self, validated_data, user):
+        with transaction.atomic():
+            utility_module = super(UtilitySubModuleSerializer, self).create(validated_data)
+            utility_module.created_by = user
+            utility_module.save()
+            return utility_module
+
+    def update(self, instance, validated_data, user):
+        with transaction.atomic():
+            utility_module = super(UtilitySubModuleSerializer, self).update(instance, validated_data)
+            utility_module.updated_by = user
+            utility_module.updated_date = timezone.now()
+            utility_module.save()
+            return utility_module
