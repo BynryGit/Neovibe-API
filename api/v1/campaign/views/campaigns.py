12b27086c5efa536607f31efc30de6fdb30eb1a2
MCCIA <kacharepriya17@gmail.com>
2020-05-18 11:28:32 +0530

    Add new code in list api

--- api/v1/campaign/views/campaigns.py
+++ api/v1/campaign/views/campaigns.py
@@ -1,17 +1,12 @@
 import traceback
 import logging
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter, SearchFilter
 from rest_framework.response import Response
-from api.messages import SUCCESS, STATE, ERROR, EXCEPTION, DATA
 from rest_framework.generics import GenericAPIView
 from rest_framework import generics, status
 from v1.commonapp.views.logger import logger
 from v1.userapp.models.user_master import UserDetail
-from v1.campaign.models.campaign_objective import get_cam_objective_by_id_string,get_cam_objective_by_id
-from v1.campaign.models.campaign_status import get_cam_status_by_tenant_id_string,get_cam_status_by_id_string
-from v1.consumer.models.consumer_category import get_consumer_category_by_id_string,get_consumer_category_by_id,get_consumer_category_by_tenant_id_string
-from v1.consumer.models.consumer_sub_category import get_consumer_sub_category_by_id_string
-from v1.commonapp.models.area import get_areas_by_tenant_id_string, get_area_by_id,get_area_by_id_string
-from v1.commonapp.models.sub_area import get_sub_area_by_id_string
 from v1.campaign.models.campaign import get_campaign_by_id_string
 from v1.commonapp.views.pagination import StandardResultsSetPagination
 from v1.campaign.models.campaign import Campaign as CampaignTbl
@@ -38,51 +33,16 @@ class CampaignList(generics.ListAPIView):
     serializer_class = CampaignListSerializer
     pagination_class = StandardResultsSetPagination
 
+    filter_backends = (DjangoFilterBackend, OrderingFilter, SearchFilter)
+    filter_fields = ('name', 'tenant__id_string','start_date',)
+    ordering_fields = ('name',)
+    ordering = ('created_date',)  # always give by default alphabetical order
+    search_fields = ('name',)
+
     def get_queryset(self):
-        try:
-            search_str = self.request.query_params.get('search', None)
+        queryset = CampaignTbl.objects.filter(is_active=True)
+        return queryset
 
-            queryset = CampaignTbl.objects.filter(is_active=True).order_by('-id')
-
-            utility_id_string = self.request.query_params.get('utility', None)
-            category_id_string = self.request.query_params.get('category', None)
-            sub_category_id_string = self.request.query_params.get('sub_category', None)
-            area_id_string = self.request.query_params.get('area', None)
-            sub_area_id_string = self.request.query_params.get('sub_area', None)
-            object_id_string = self.request.query_params.get('object', None)
-            status_id_string = self.request.query_params.get('status', None)
-
-            if utility_id_string is not None:
-                queryset = queryset.filter(utility__id_string=utility_id_string)
-            if category_id_string is not None:
-                category = get_consumer_category_by_id_string(category_id_string)
-                queryset = queryset.filter(category_id=category.id)
-            if sub_category_id_string is not None:
-                sub_category = get_consumer_sub_category_by_id_string(sub_category_id_string)
-                queryset = queryset.filter(sub_category_id=sub_category.id)
-            if area_id_string is not None:
-                area = get_area_by_id_string(area_id_string)
-                queryset = queryset.filter(area_id=area.id)
-            if sub_area_id_string is not None:
-                sub_area = get_sub_area_by_id_string(sub_area_id_string)
-                queryset = queryset.filter(sub_area_id=sub_area.id)
-            if object_id_string is not None:
-                objective = get_cam_objective_by_id_string(object_id_string)
-                queryset = queryset.filter(objective_id=objective.id)
-            if status_id_string is not None:
-                status = get_cam_status_by_id_string(status_id_string)
-                queryset = queryset.filter(status_id=status.id)
-
-            if search_str is not None:
-                queryset = CampaignTbl.objects.filter(is_active=True, name__icontains=search_str).order_by('-id')
-
-            return queryset
-        except Exception as e:
-            logger().log(e, 'ERROR', user='test', name='test')
-            return Response({
-                STATE: EXCEPTION,
-                DATA: '',
-            }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 # API Header
