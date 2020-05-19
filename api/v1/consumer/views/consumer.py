80bbf5e2cd50044bad2dd3f8477946f1bc12cb34
Rohan Wagh <waghrohan218@gmail.com>
2020-05-19 16:48:52 +0530

    consumer api

--- api/v1/consumer/views/consumer.py
+++ api/v1/consumer/views/consumer.py
@@ -1,14 +1,25 @@
 from rest_framework import status
 from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
-from api.messages import EXCEPTION, STATE, ERROR, RESULTS, SUCCESS
+from api.messages import EXCEPTION, STATE, ERROR, RESULTS, SUCCESS, DATA
 from v1.commonapp.common_functions import is_authorized, is_token_valid
 from v1.commonapp.views.logger import logger
+from v1.consumer.models.consumer_master import get_consumer_by_id_string
 from v1.consumer.serializers.consumer import ConsumerSerializer, ConsumerViewSerializer
 from v1.registration.views.common_functions import is_data_verified
 from v1.userapp.models.user_master import UserDetail
 
-
+# API Header
+# API end Point: api/v1/consumer
+# API verb: POST
+# Package: Basic
+# Modules: S&M, Consumer Care, Consumer Ops
+# Sub Module: Consumer
+# Interaction: Add
+# Usage: Add
+# Tables used: ConsumerMaster
+# Auther: Rohan
+# Created on: 19/05/2020
 class Consumer(GenericAPIView):
 
     def post(self, request):
@@ -49,6 +60,102 @@ class Consumer(GenericAPIView):
             else:
                 return Response({
                     STATE: ERROR,
+                }, status=status.HTTP_401_UNAUTHORIZED)
+        except Exception as e:
+            logger().log(e, 'ERROR', user='test', name='test')
+            return Response({
+                STATE: EXCEPTION,
+                ERROR: ERROR
+            }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+# API Header
+# API end Point: api/v1/consumer/:id_string
+# API verb: GET, PUT
+# Package: Basic
+# Modules: S&M, Consumer Care, Consumer Ops
+# Sub Module: Consumer
+# Interaction: Get, Update
+# Usage: Add
+# Tables used: ConsumerMaster
+# Auther: Rohan
+# Created on: 19/05/2020
+class ConsumerDetail(GenericAPIView):
+
+    def get(self, request, id_string):
+        try:
+            if is_token_valid(self.request.headers['token']):
+                if is_authorized():
+                    consumer = get_consumer_by_id_string(id_string)
+                    if consumer:
+                        serializer = ConsumerViewSerializer(instance=consumer, context={'request': request})
+                        return Response({
+                            STATE: SUCCESS,
+                            DATA: serializer.data,
+                        }, status=status.HTTP_200_OK)
+                    else:
+                        return Response({
+                            STATE: EXCEPTION,
+                            DATA: '',
+                        }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+                else:
+                    return Response({
+                        STATE: ERROR,
+                    }, status=status.HTTP_403_FORBIDDEN)
+            else:
+                return Response({
+                    STATE: ERROR,
+                }, status=status.HTTP_401_UNAUTHORIZED)
+        except Exception as e:
+            logger().log(e, 'ERROR', user='test', name='test')
+            return Response({
+                STATE: EXCEPTION,
+                DATA: '',
+            }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    def put(self, request, id_string):
+        try:
+            # Checking authentication start
+            if is_token_valid(request.headers['token']):
+                # Checking authentication end
+
+                # Checking authorization start
+                if is_authorized():
+                    # Checking authorization end
+
+                    # Request data verification start
+                    if is_data_verified(request):
+                        # Request data verification end
+
+                        # Save basic details start
+                        user = UserDetail.objects.get(id=2)
+                        consumer = get_consumer_by_id_string(id_string)
+                        if consumer:
+                            serializer = ConsumerSerializer(data=request.data)
+                            if serializer.is_valid(request.data):
+                                consumer_obj = serializer.update(consumer, serializer.validated_data, user)
+                                view_serializer = ConsumerViewSerializer(instance=consumer_obj,
+                                                                             context={'request': request})
+                                return Response({
+                                    STATE: SUCCESS,
+                                    RESULTS: view_serializer.data,
+                                }, status=status.HTTP_200_OK)
+                        else:
+                            return Response({
+                                STATE: ERROR,
+                            }, status=status.HTTP_404_NOT_FOUND)
+                        # Save basic details start
+                    else:
+                        return Response({
+                            STATE: ERROR,
+                        }, status=status.HTTP_400_BAD_REQUEST)
+                else:
+                    return Response({
+                        STATE: ERROR,
+                    }, status=status.HTTP_403_FORBIDDEN)
+            else:
+                return Response({
+                    STATE: ERROR,
+
                 }, status=status.HTTP_401_UNAUTHORIZED)
         except Exception as e:
             # logger().log(e, 'ERROR', user='test', name='test')
