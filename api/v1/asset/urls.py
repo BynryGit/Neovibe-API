fa9a6b8594375fd56a1e086eb8842da9b05cfe29
MCCIA <kacharepriya17@gmail.com>
2020-05-22 16:09:03 +0530

    only taking pull

--- api/v1/asset/urls.py
+++ api/v1/asset/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from v1.asset.views.asset import AssetList,AssetDetail,Asset
+from v1.asset.views.asset_status import AssetstatusList,AssetStatusDetail
 
 urlpatterns = [
     path('list', AssetList.as_view(), name="asset_list"),
     path('', Asset.as_view(), name="asset"),
     path('<uuid:id_string>', AssetDetail.as_view(), name="asset_detail"),
+
+    path('status-list', AssetstatusList.as_view(), name="asset_status_list"),
+    path('status/<uuid:id_string>', AssetStatusDetail.as_view(), name="asset_status_detail"),
 ]
 
