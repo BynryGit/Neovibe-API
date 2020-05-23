d70ff095194de3e85124c99026dd53dd3cb8f12f
MCCIA <kacharepriya17@gmail.com>
2020-05-23 17:54:37 +0530

    taking pull

--- api/v1/dispatcher/models/service_type.py
+++ api/v1/dispatcher/models/service_type.py
@@ -1,16 +1,3 @@
-# Table Header
-# module: O&M  | sub-module - SOP
-# Table Type : Lookup (Local)
-# Table Name : 2.12.77 Task Type (Local)
-# Description : A lookup table for types of task added in SOP's.
-# Frequency of data changes : Low
-# Sample Table Data : Image, Parameter, Material
-# Reference Table : 2.6.3 SOP Master Details
-# Author : Jayshree Kumbhare
-# Creation Date : 22-04-2020
-
-# change history
-# <ddmmyyyy>-<changes>-<Author>
 
 from datetime import datetime # importing package for datetime
 from v1.tenant.models.tenant_master import TenantMaster
@@ -19,9 +6,9 @@ import uuid  # importing package for GUID
 from django.db import models  # importing package for database
 
 
-# Create Task Type table start
+# Create Service Type table start
 
-class TaskType(models.Model):
+class ServiceTypes(models.Model):
     id_string = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
     utility = models.ForeignKey(UtilityMaster, blank=True, null=True, on_delete=models.SET_NULL)
     name = models.CharField(max_length=200, blank=False, null=False)
