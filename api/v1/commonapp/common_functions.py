19d873e3d7f38e0148c4361bdbc5996e2eef2cb0
arpita <arpita.badwaik@bynry.com>
2020-05-23 11:46:17 +0530

    is authorized API

--- api/v1/commonapp/common_functions.py
+++ api/v1/commonapp/common_functions.py
@@ -2,7 +2,9 @@ import jwt # jwt token library
 from api.settings import SECRET_KEY
 from v1.commonapp.views.custom_exception import InvalidAuthorizationException, InvalidTokenException
 from v1.commonapp.views.logger import logger
+from v1.userapp.models.role_privilege import get_record_by_values, get_record_values_by_id
 from v1.userapp.models.user_master import get_user_by_id_string
+from v1.userapp.models.user_role import get_user_role_by_user_id
 from v1.userapp.models.user_token import get_token_by_user_id
 
 
@@ -46,11 +48,11 @@ def is_authorized():
 #     try:
 #         data = False
 #         decoded_token = get_payload(token)
-#         user_obj = get_user_by_id_string(str(decoded_token))
+#         user_obj = get_user_by_id_string(str(decoded_token['id_string']))
 #         roles = get_user_role_by_user_id(user_obj.id)
 #         if roles:
 #             for role in roles:
-#                 privilege = get_record_by_values(role.id,module_id,sub_module_id,privilege_id)
+#                 privilege = get_record_values_by_id(role.id,module_id,sub_module_id,privilege_id)
 #                 if privilege:
 #                     data = True
 #                     return data
@@ -61,4 +63,4 @@ def is_authorized():
 #             return data
 #     except Exception as e:
 #         logger().log(e, 'ERROR', user='test', name='test')
-#         raise InvalidAuthorizationException
+#         return False
