f85b41aa0fec2924410e028a5833a0cdc74469bf
arpita <arpita.badwaik@bynry.com>
2020-06-23 16:20:10 +0530

    test case

--- api/api/settings.py
+++ api/api/settings.py
@@ -102,9 +102,9 @@ WSGI_APPLICATION = 'api.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'smart360',
+        'NAME': 'smart3603',
         'USER': 'postgres',
-        'PASSWORD': 'root',
+        'PASSWORD': 'admin',
         'HOST': 'localhost',
         'PORT': '5432',
     }
