8ead933377af80e54d8f190e60e5b51f2fbe8c5b
priyanka <kacharepriya17@gmail.com>
2021-01-28 10:37:00 +0530

    write api for take a data from consumer meter

--- api/v1/consumer/serializers/consumer_meter.py
+++ api/v1/consumer/serializers/consumer_meter.py
@@ -1,12 +1,13 @@
 from rest_framework import serializers
 from v1.consumer.models.consumer_meter import ConsumerMeter
 from v1.meter_data_management.serializers.meter import MeterListSerializer
-
+from v1.consumer.serializers.consumer_master import ConsumerListSerializer
 
 class ConsumerMeterListSerializer(serializers.ModelSerializer):
     meter = MeterListSerializer(source='get_meter')
+    consumer = ConsumerListSerializer(source='get_consumer')
 
     class Meta:
         model = ConsumerMeter
-        fields = ('id_string', 'meter')
+        fields = ('id_string', 'meter','consumer')
 
---
+++ api/v1/userapp/serializers/field_agent_live_location.py
@@ -0,0 +1,19 @@
+from django.db import transaction
+from datetime import datetime
+from v1.userapp.serializers.user import GetUserSerializer
+from rest_framework import serializers, status
+
+from api.settings.prod import DISPLAY_DATE_TIME_FORMAT
+from v1.commonapp.views.custom_exception import CustomAPIException
+from v1.userapp.models.user_utility import UserUtility
+from v1.userapp.views.common_functions import set_user_utility_validated_data
+from v1.utility.serializers.utility import UtilitySerializer
+from v1.userapp.models.field_agent_live_location import FieldAgentLiveLocation
+
+class FieldAgentLiveLocationViewSerializer(serializers.ModelSerializer):
+
+    utility = UtilitySerializer(many=False, required=True, source='get_utility')
+    user_id = GetUserSerializer(many=False, required=True, source='get_user')
+    class Meta:
+        model = FieldAgentLiveLocation
+        fields = ('__all__')
