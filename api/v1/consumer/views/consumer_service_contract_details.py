117eb6f955222d5179f0e6bd05f993cf78cdf34d
binary-ibex <70746835+binary-ibex@users.noreply.github.com>
2021-02-22 11:54:25 +0530

    add the filter for fetching the meter data

--- api/v1/consumer/views/consumer_service_contract_details.py
+++ api/v1/consumer/views/consumer_service_contract_details.py
@@ -10,24 +10,24 @@ from v1.consumer.models.consumer_master import get_consumer_by_id_string
 from v1.consumer.models.consumer_service_contract_details import ConsumerServiceContractDetail
 from v1.consumer.serializers.consumer_service_contract_details import ConsumerServiceContractDetailViewSerializer
 from v1.meter_data_management.models.meter import get_meter_by_id_string
-
+from v1.utility.models.utility_master import get_utility_by_id_string
+from v1.commonapp.views.custom_filter_backend import CustomFilter
 
 class ConsumerServiceContractDetailList(generics.ListAPIView):
     try:
         serializer_class = ConsumerServiceContractDetailViewSerializer
         pagination_class = StandardResultsSetPagination
-
         filter_backends = (DjangoFilterBackend, OrderingFilter, SearchFilter)
         filter_fields = ('tenant__id_string',)
         ordering_fields = ('tenant',)
         search_fields = ('tenant__name',)
-
         def get_queryset(self):
             response, user_obj = is_token_valid(self.request.headers['Authorization'])
             if response:
                 if is_authorized(1, 1, 1, user_obj):
-                    consumer = get_consumer_by_id_string(self.kwargs['id_string'])
-                    queryset = ConsumerServiceContractDetail.objects.filter(consumer_id=consumer.id, is_active=True)
+                    utility = get_utility_by_id_string(self.kwargs['id_string'])
+                    queryset = ConsumerServiceContractDetail.objects.filter(utility=utility, is_active=True)
+                    queryset = CustomFilter.get_filtered_queryset(queryset, self.request)
                     if queryset:
                         return queryset
                     else:
@@ -38,7 +38,6 @@ class ConsumerServiceContractDetailList(generics.ListAPIView):
                 raise InvalidTokenException
     except Exception as e:
         logger().log(e, 'MEDIUM', module='Consumer ops', sub_module='consumer')
-
 # Note: rohan below is updated api for fetch service contract detail list you just delete above code give class name to below api and delete url
 
 
