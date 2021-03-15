e4ccb5ed33106ae73c3356610832718d0c979529
Akshay <akshay.nibrad@bynry.com>
2021-03-15 18:04:07 +0530

    dependancy file edit suggested by gaurav

--- api/v1/meter_data_management/serializers/validation_schedule_log.py
+++ api/v1/meter_data_management/serializers/validation_schedule_log.py
@@ -1,6 +1,8 @@
 __author__ = "aki"
 
 from rest_framework import serializers
+
+from v1.commonapp.models.meter_status import get_meter_status_by_name
 from v1.commonapp.serializers.tenant import TenantMasterViewSerializer
 from v1.commonapp.serializers.utility import UtilityMasterViewSerializer
 from v1.meter_data_management.models.schedule_log import ScheduleLog as ScheduleLogTbl
@@ -26,6 +28,7 @@ class ValidationScheduleLogViewSerializer(serializers.ModelSerializer):
 
     def get_meter_reading_detail(self, schedule_log_tbl):
         total_completed_task = 0
+        meter_status_obj = get_meter_status_by_name('RCNT')
         consumer_obj = ConsumerDetailTbl.objects.filter(schedule_log_id=schedule_log_tbl.id, is_active=True).count()
         meter_reading_obj = MeterReadingTbl.objects.filter(schedule_log_id=schedule_log_tbl.id)
         route_task_assignment_obj = RouteTaskAssignmentTbl.objects.filter(schedule_log_id=schedule_log_tbl.id,
@@ -37,11 +40,11 @@ class ValidationScheduleLogViewSerializer(serializers.ModelSerializer):
             total_completed_task = total_completed_task + len(complete_task_obj)
 
         meter_reading_detail = {
-            'Total_consumer' : consumer_obj,
-            'reading_received' : total_completed_task,
+            'total_consumer' : consumer_obj,
+            'received_reading' : total_completed_task,
             'pending_reading' : consumer_obj - total_completed_task,
-            'total_revisit' : meter_reading_obj.filter(reading_status=3, is_active=False).count(),
-            'total_duplicate' : meter_reading_obj.filter(is_duplicate=True, is_active=False).count(),
+            'rcnt_reading' : meter_reading_obj.filter(reading_status=2, meter_status_id=meter_status_obj.id, is_active=True).count(),
+            'duplicate_reading' : meter_reading_obj.filter(is_duplicate=True, is_active=False).count(),
             'validation_one' : meter_reading_obj.filter(reading_status=0, is_active=True).count(),
             'validation_two' : meter_reading_obj.filter(reading_status=1, is_assign_to_v1=True, is_active=True).count(),
             'completed_reading': meter_reading_obj.filter(reading_status=2, is_assign_to_v2=True, is_active=True).count(),
