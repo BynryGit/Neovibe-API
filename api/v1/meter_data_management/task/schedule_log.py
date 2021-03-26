73a076182ce9a319a604fed646ab64952b9b91a4
Akshay <akshay.nibrad@bynry.com>
2021-03-26 16:45:06 +0530

    update schedule log job

--- api/v1/meter_data_management/task/schedule_log.py
+++ api/v1/meter_data_management/task/schedule_log.py
@@ -5,96 +5,50 @@ __author__ = "aki"
 # Modules: All
 # Sub Module: All
 # Usage: This task is used to save schedule log
-# Tables used: Schedule, ScheduleLog, Global Lookup
+# Tables used: Schedule, ScheduleLog
 # Author: Akshay
-# Created on: 26/02/2021
+# Created on: 26/03/2021
 
-from django.utils import timezone
+from datetime import datetime
+from croniter import croniter
 from django.db import transaction
 from django.db.models import Q
 from v1.commonapp.views.logger import logger
-from v1.commonapp.models.global_lookup import get_global_lookup_by_id
-from v1.meter_data_management.models.schedule_log import ScheduleLog
-from v1.meter_data_management.models.schedule import Schedule
+from v1.meter_data_management.models.schedule_log import ScheduleLog as ScheduleLogTbl
+from v1.meter_data_management.models.schedule import Schedule as ScheduleTbl
 from v1.meter_data_management.task.consumer_detail import create_consumer
 
 
 def schedule_log():
     try:
-        current_date = timezone.now()
-        schedule_obj = Schedule.objects.filter(Q(end_date__date__gte=current_date.date()) |
+        schedule_obj = ScheduleTbl.objects.filter(Q(end_date__date__gte=datetime.now().date()) |
                                                Q(end_date__date=None),
-                                               start_date__date__lte=current_date.date(), is_active=True)
+                                               start_date__date__lte=datetime.now().date(), is_active=True)
         for schedule in schedule_obj:
-            recurrence_obj = get_global_lookup_by_id(schedule.recurring_id)
-            if recurrence_obj.key == 'no':
-                if ScheduleLog.objects.filter(tenant=schedule.tenant, utility=schedule.utility, schedule_id=schedule.id,
-                                              is_active=True).exists():
-                    print("Schedule Already Exist")
-                else:
-                    create_schedule_log(schedule)
+            croniter_obj = croniter.match(schedule.cron_expression, datetime(datetime.today().year,
+                                                                             datetime.today().month,
+                                                                             datetime.today().day, 22,00,00))
+            if croniter_obj:
+                with transaction.atomic():
+                    if ScheduleLogTbl.objects.filter(tenant=schedule.tenant, utility=schedule.utility,
+                                                     schedule_id=schedule.id,
+                                                     date_and_time__date=datetime.now().date()).exists():
+                        print("Schedule Log Already Exist")
+                    else:
+                        schedule_log_obj = ScheduleLogTbl(tenant=schedule.tenant, utility=schedule.utility,
+                                                          schedule_id=schedule.id, read_cycle_id=schedule.read_cycle_id,
+                                                          activity_type_id=schedule.activity_type_id,
+                                                          recurring_id=schedule.recurring_id,
+                                                          utility_product_id=schedule.utility_product_id,
+                                                          date_and_time=datetime.now())
+                        schedule_log_obj.save()
+                        # Call Consumer Import Job
+                        create_consumer.delay(schedule_log_obj.id)
             else:
-                frequency_obj = get_global_lookup_by_id(schedule.frequency_id)
-                repeat_every_obj = get_global_lookup_by_id(schedule.repeat_every_id)
-                if frequency_obj.key == "hourly":
-                    if repeat_every_obj.key == "1 hour":
-                        if ScheduleLog.objects.filter(tenant=schedule.tenant, utility=schedule.utility,
-                                                      schedule_id=schedule.id, date_and_time__hour=current_date.hour,
-                                                      is_active=True).exists():
-                            print("Schedule For Hour Is Already Exist")
-                        else:
-                            create_schedule_log(schedule)
-                    elif repeat_every_obj.key == "2 hour":
-                        pass
-                    elif repeat_every_obj.key == "3 hour":
-                        pass
-                    elif repeat_every_obj.key == "4 hour":
-                        pass
-                    elif repeat_every_obj.key == "5 hour":
-                        pass
-                elif frequency_obj.key == "daily":
-                    if repeat_every_obj.key == "1 day":
-                        if ScheduleLog.objects.filter(tenant=schedule.tenant, utility=schedule.utility,
-                                                      schedule_id=schedule.id, date_and_time__date=current_date.date(),
-                                                      is_active=True).exists():
-                            print("Schedule For Current Date Is Already Exist")
-                        else:
-                            create_schedule_log(schedule)
-                    elif repeat_every_obj.key == "2 day":
-                        pass
-                    elif repeat_every_obj.key == "3 day":
-                        pass
-                    elif repeat_every_obj.key == "4 day":
-                        pass
-                    elif repeat_every_obj.key == "5 day":
-                        pass
-                elif frequency_obj.key == "yearly":
-                    if repeat_every_obj.key == "1 year":
-                        if ScheduleLog.objects.filter(tenant=schedule.tenant, utility=schedule.utility,
-                                                      schedule_id=schedule.id, date_and_time__year=current_date.year,
-                                                      is_active=True).exists():
-                            print("Schedule For Hour Is Already Exist")
-                        else:
-                            create_schedule_log(schedule)
-                    elif repeat_every_obj.key == "2 year":
-                        pass
+                print("Cron Expression Not Match")
     except Exception as ex:
+        print(ex)
         logger().log(ex, 'MEDIUM', module='CONSUMER OPS', sub_module='METER DATA')
 
 
 schedule_log()
-
-
-def create_schedule_log(schedule):
-    current_date = timezone.now()
-    with transaction.atomic():
-        try:
-            schedule_log_obj = ScheduleLog(tenant=schedule.tenant, utility=schedule.utility, schedule_id=schedule.id,
-                                           read_cycle_id=schedule.read_cycle_id, activity_type_id=schedule.activity_type_id,
-                                           recurring_id=schedule.recurring_id,
-                                           utility_product_id=schedule.utility_product_id, date_and_time=current_date)
-            schedule_log_obj.save()
-            create_consumer.delay(schedule_log_obj.id)
-        except Exception as ex:
-            print(ex)
-            logger().log(ex, 'MEDIUM', module='CONSUMER OPS', sub_module='METER DATA')
