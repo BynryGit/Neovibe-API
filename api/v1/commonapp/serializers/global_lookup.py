4cfb7b6d5a24afe0b96de1b8546b79b5c1badba0
Akshay <akshay.nibrad@bynry.com>
2021-05-07 18:45:40 +0530

    schedule bugs

--- api/v1/commonapp/serializers/global_lookup.py
+++ api/v1/commonapp/serializers/global_lookup.py
@@ -9,7 +9,7 @@ class GlobalLookupShortViewSerializer(serializers.ModelSerializer):
         if global_lookup_tbl:
             value = global_lookup_tbl.value
         else:
-            value= '------'
+            value = '-'
         return value
 
     class Meta:
